e40e5e7017e8a55cee0cd5750a657e57b09b6090
hj7321 <oboki2001@naver.com>
2024-09-26 14:41:30 +0900

    [Script] 초기 세팅

---
+++ src/components/ModalOrderModifyCard.tsx
@@ -0,0 +1,7 @@
+export default function ModalOrderModifyCard() {
+  return (
+    <div>
+      여행 계획 페이지에서 오버레이 하면 나오는 순서 수정 카드 모달 컴포넌트
+    </div>
+  );
+}
---
+++ src/shared/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header>공통 레이아웃-헤더 부분</header>;
+}
